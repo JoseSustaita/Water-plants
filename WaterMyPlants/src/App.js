c177dbbdfd12db8c1c3fa798a277d2248a2f4240
Jose Sustaita <josesustaita70@gmail.com>
2020-05-24 18:13:46 -0400

    Organized components & added new routes

--- WaterMyPlants/src/App.js
+++ WaterMyPlants/src/App.js
@@ -1,21 +1,30 @@
 import React from "react";
 import "./App.css";
-import Login from "./Components/Login";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Signup from "./Components/Signup";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+//Component links
+import Login from "./Components/Login_Components/Login";
+import Signup from "./Components/Login_Components/Signup";
 import PlantCreation from "./Components/Plant_Components/PlantCreation";
-import Plants from "./Components/Plant_Components/Plants"
+import Plants from "./Components/Plant_Components/Plants";
+import Home from "./Components/Home_Components/Home";
+import HomeAllPlants from "./Components/Home_Components/HomeAllPlants";
 
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/HomeAllPlants">Home</Link>
+      </nav>
       <div className="App">
         <Switch>
-          <Route exact path="/" component={Login} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Login" component={Login} />
           <Route exact path="/Signup" component={Signup} />
+          <Route exact path="/HomeAllPlants" component={HomeAllPlants} />
           <Route exact path="/PlantCreation" component={PlantCreation} />
         </Switch>
-        <Plants/>
+        <Plants />
       </div>
     </Router>
   );
