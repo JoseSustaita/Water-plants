c04352801464926c97011c10b69d879092b40cba
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2020-05-26 16:48:31 -0400

    Home Page W no Image

--- WaterMyPlants/src/Components/Home_Components/Home.js
+++ WaterMyPlants/src/Components/Home_Components/Home.js
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import Button from "react-bootstrap/Button";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+
 const Home = () => {
+  const backgroundStyle = {
+    color: "Green",
+  };
   return (
-    <div>
-      <h1>Planted Parenthood</h1>
-      <Link to="/Login">Click Here To Get Started!</Link>
+    <div style={backgroundStyle}>
+      <Jumbotron fluid>
+        <Container>
+          <h1>Welcome To Plants Watered</h1>
+          <Button variant="primary">Get Started</Button>
+          <h2>Never Forget To Water Your Plants Again</h2>
+        </Container>
+      </Jumbotron>
     </div>
   );
 };
